c09cbfe08005053edc13b51fdd0f4c0713b10b2b
visala29 <117522609+visala29@users.noreply.github.com>
2023-04-17 14:39:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello Visala")
+
+
+def welcome():
+  print("Welcome to github actions")
+  
+  
+print("This is for learning github actions")
+welcome();                                                                                                                                                                                                                   
